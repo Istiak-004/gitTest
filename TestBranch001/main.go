0bab7a6f3f0886324c1e966d67831f5ab4fe5a04
Istiak-004 <whitedeath1208004@gmail.com>
2021-06-17 22:22:50 +0530

    This is the third testing branch
    [TEST-02]

--- TestBranch001/main.go
+++ TestBranch001/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main(){
 	val :="in branch 02"
+	val2 :="change 3"
 	fmt.Println("This is a test branch")
 	fmt.Printf("this is the second change %+v",val)
+	fmt.Println(val2)
 }
